e09f9d30e581a2ea60a07ef1ad52f9d444b729d2
tyronejose <alt.tyronejose@gmail.com>
2024-10-04 13:10:07 -0300

    feat(products): add filters for search, brand, category and sort_by to products endpoint

---
+++ app/modules/products/choices.py
@@ -0,0 +1,9 @@
+"""Choices for Products App."""
+
+
+class SortChoices(str):
+
+    LATEST = "latest"
+    OLDEST = "oldest"
+    HIGHEST_PRICE = "highest_price"
+    LOWEST_PRICE = "lowest_price"
--- app/modules/products/routes.py
+++ app/modules/products/routes.py
@@ -1,10 +1,11 @@
 """Routes for Products App."""
 
-from typing import List
+from typing import List, Optional
 from fastapi import APIRouter, Query, HTTPException
 
 from app.utils.helpers import load_json
 from .schemas import Brand, Category, ProductMinimal, ProductPaginated
+from .choices import SortChoices
 
 router = APIRouter()
 
@@ -35,28 +36,71 @@ async def get_categories():
     tags=["Products"],
 )
 async def get_products(
+    sort_by: Optional[str] = Query(
+        None,
+        enum=[
+            SortChoices.LATEST,
+            SortChoices.OLDEST,
+            SortChoices.HIGHEST_PRICE,
+            SortChoices.LOWEST_PRICE,
+        ],
+    ),
+    search: Optional[str] = Query(None),
+    brand: Optional[str] = Query(None),
+    category: Optional[str] = Query(None),
     page: int = Query(1, gt=0),
 ):
     products_data = load_json("app/data/products.json")
+
+    # Sort_by filter
+    # ! TODO: Fix id by created_at field
+    if sort_by == SortChoices.LATEST:
+        products_data = sorted(products_data, key=lambda x: x["id"], reverse=True)
+    elif sort_by == SortChoices.OLDEST:
+        products_data = sorted(products_data, key=lambda x: x["id"])
+    elif sort_by == SortChoices.HIGHEST_PRICE:
+        products_data = sorted(products_data, key=lambda x: x["price"], reverse=True)
+    elif sort_by == SortChoices.LOWEST_PRICE:
+        products_data = sorted(products_data, key=lambda x: x["price"])
+
+    # Search filter
+    if search:
+        products_data = [
+            item for item in products_data if search.lower() in item["name"].lower()
+        ]
+
+    # Brand filter
+    if brand:
+        products_data = [
+            item for item in products_data if item["brand"].lower() == brand.lower()
+        ]
+
+    # Category filter
+    if category:
+        products_data = [
+            item
+            for item in products_data
+            if item["category"].lower() == category.lower()
+        ]
+
+    products = [ProductMinimal(**item) for item in products_data]
+    BASE_URL = "http://127.0.0.1:8100/api/"  # TODO: Add .env
+
+    # Pagination
     page_size = 10
     start = (page - 1) * page_size
     end = start + page_size
-
-    products = [ProductMinimal(**item) for item in products_data]
-    page_products = products[start:end]
+    results = products[start:end]
     count = len(products)
-    next_page = (
-        f"http://127.0.0.1:8100/api/products?page={page + 1}" if end < count else None
-    )
-    previous_page = (
-        f"http://127.0.0.1:8100/api/products?page={page - 1}" if page > 1 else None
-    )
+
+    next_page = f"{BASE_URL}products?page={page + 1}" if end < count else None
+    previous_page = f"{BASE_URL}products?page={page - 1}" if page > 1 else None
 
     return {
         "count": count,
         "next": next_page,
         "previous": previous_page,
-        "results": page_products,
+        "results": results,
     }
 
 
