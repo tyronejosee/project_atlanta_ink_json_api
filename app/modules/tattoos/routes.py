0dc60e3113fb59edfaf7ffe00c97983456b30b7e
tyronejose <alt.tyronejose@gmail.com>
2024-10-09 14:14:08 -0300

    feat(tattoos): add random tattoo endpoint

--- app/modules/tattoos/routes.py
+++ app/modules/tattoos/routes.py
@@ -1,5 +1,6 @@
 """Routes for Tattoos App."""
 
+import random
 from typing import List
 from fastapi import APIRouter
 
@@ -17,3 +18,17 @@ router = APIRouter()
 async def get_tattoos():
     tattoos_data = load_json("app/data/tattoos.json")
     return tattoos_data
+
+
+@router.get(
+    "/tattoos/random",
+    response_model=List[Tattoo],
+    tags=["Tattoos"],
+)
+async def get_random_tattoos():
+    tattoos_data = load_json("app/data/tattoos.json")
+    random_tattoos = random.sample(
+        tattoos_data,
+        k=min(12, len(tattoos_data)),
+    )
+    return random_tattoos
